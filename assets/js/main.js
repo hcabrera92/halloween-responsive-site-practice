d4508a3fea0f0e898053cbc91d3388a12c6e50ea
hcabrera92 <humbertocabrera1992@gmail.com>
2021-10-31 18:34:04 -0500

    Added Nav Bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,23 @@
-/*=============== SHOW MENU ===============*/
+/*==================== SHOW MENU ====================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
@@ -16,7 +35,7 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
