6973ec547915d33054f6eb634a2128bdcdfae08d
hcabrera92 <humbertocabrera1992@gmail.com>
2021-11-02 02:22:44 -0500

    Finished main Home Section also I learnt how to add the Swiper JS to my project.

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,14 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== HOME SWIPER ===============*/
-
+let homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+      
+    pagination: {
+      el: ".swiper-pagination",
+    },
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
