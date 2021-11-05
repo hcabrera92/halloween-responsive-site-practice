94b68e8b2c70826f6d3911c0159eb3402d630586
hcabrera92 <humbertocabrera1992@gmail.com>
2021-11-05 12:28:49 -0500

    Backgorund now changes whem scroll is less than 50

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,7 +41,12 @@ let homeSwiper = new Swiper(".home-swiper", {
 });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== NEW SWIPER ===============*/
 
